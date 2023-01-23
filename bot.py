bdd1a9901a683cb223491f4fe94e72d69c8de06e
LYNX1239038120 <51261630+Yuyichn01@users.noreply.github.com>
2023-01-23 13:13:02 -0500

    Update bot.py
    added new function

--- bot.py
+++ bot.py
@@ -1,30 +1,46 @@
-import slack_sdk
+from cgitb import text
+from multiprocessing import Event
 import os
+import time
+from typing import Text
+import slack_sdk
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
-from slack_sdk import WebClient
+from slack_bolt import App
+import logging
+from datetime import datetime
 from slackeventsapi import SlackEventAdapter
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-#slack bot initialization
-#change ngrok-io during launch
-env_path = Path('.')/ '.env'
-load_dotenv(dotenv_path = env_path)
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
-client = slack_sdk.WebClient(token=os.environ['SLACK_TOKEN'])
-BOT_ID = client.api_call("auth.test")['user_id']
+#initialize bolt environment variables
+env_path = Path('.')/'.env'
+load_dotenv(dotenv_path=env_path)
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
+channel1 = os.environ["channel_id1"]
+conversation_history = []
+logger = logging.getLogger(__name__)
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'])
 
-#slack message-send-back function
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event',{})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
-    if BOT_ID != user_id:
-      client.chat_postMessage(channel=channel_id, text=text)
-      
+#listen to keyword "parking" and trigger events
+@app.message("parking")
+def message1(message, say):
 
+    # welcome message
+    say(f"Welcome to parking assistant <@{message['user']}>! ")
+    say(f"Please enter your desired parking number (1 to 14): ")
+
+@app.event("app_mention",)
+def event1(say):
+    say("Hi there!")
+    say("what can i help you? ")
+    
+        
+    
+
+# Start app
 if __name__ == "__main__":
-        app.run(debug=True)
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+ 
