9d9ee985b8a3c3f235f58def1fb93385700af66a
LYNX1239038120 <51261630+Yuyichn01@users.noreply.github.com>
2023-01-29 10:54:13 -0500

    Update bot.py
    refinement of code

--- bot.py
+++ bot.py
@@ -1,50 +1,29 @@
 import os
 from pathlib import Path
-from pickle import NONE
+import datetime
+import time
+import logging
 from dotenv import load_dotenv
 from slack_bolt import App
 from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # parking information class
 class staffParkingInfo:
-    name="empty"
-    parkingNumber = 0
+    def __init__(self, name, parkingNumber):
+     self.name = name
+     self.parkingNumber = parkingNumber
 
 #create a array of staffParkingInfo class
-p1 = staffParkingInfo() 
-p2 = staffParkingInfo() 
-p3 = staffParkingInfo() 
-p4 = staffParkingInfo() 
-p5 = staffParkingInfo() 
-p6 = staffParkingInfo() 
-p7 = staffParkingInfo() 
-p8 = staffParkingInfo() 
-p9 = staffParkingInfo() 
-p10 = staffParkingInfo() 
-p11 = staffParkingInfo() 
-p12 = staffParkingInfo() 
-p13 = staffParkingInfo() 
-p14 = staffParkingInfo() 
-parkingInfoArray = [p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14]
-
-p1.parkingNumber = 1
-p2.parkingNumber = 2
-p3.parkingNumber = 3
-p4.parkingNumber = 4
-p5.parkingNumber = 5
-p6.parkingNumber = 6
-p7.parkingNumber = 7
-p8.parkingNumber = 8
-p9.parkingNumber = 9
-p10.parkingNumber = 10
-p11.parkingNumber = 11
-p12.parkingNumber = 12
-p13.parkingNumber = 13
-p14.parkingNumber = 14
-
-for i in parkingInfoArray:
-    print(i.parkingNumber)
+parkingInfoArrayToday = []
+parkingInfoArrayTomorrow = []
+parkingInfoArrayEmpty = []
+indexArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+for i in indexArray:
+    parkingInfoArrayToday.append(staffParkingInfo("empty",i))
+    parkingInfoArrayTomorrow.append(staffParkingInfo("empty",i))
+    parkingInfoArrayEmpty.append(staffParkingInfo("empty",i))
 
 #initialize bolt environment variables
 env_path = Path('.')/'.env'
@@ -52,7 +31,8 @@ load_dotenv(dotenv_path=env_path)
 app = App(token=os.environ["SLACK_BOT_TOKEN"])
 client = WebClient(token=os.environ.get("SLACK_BOT_TOKEN"))
 channel1 = os.environ["channel_id1"]
- 
+logger = logging.getLogger(__name__)
+
 #when the user @the robot
 @app.event("app_mention")
 def event1(event,say):
@@ -66,15 +46,13 @@ def message1(message,say):
     text = message["text"]
     say(f"Hello! What can I help you?")
 
-
-    
 #reserve parking method
 @app.message("reserve parking")
 def message1(message,say):
     num = 0
     text = message["text"]
     noAvailableSpots = True
-    for i in parkingInfoArray:
+    for i in parkingInfoArrayTomorrow:
       if i.name == "empty":
         num = str(i.parkingNumber)
         i.name = text[16:]
@@ -86,20 +64,29 @@ def message1(message,say):
         say(f"you have successfully reserved badge number "+ num + ", please collect your badge at IT room")
 
 #print parking information method   
-@app.message("^parking information$")   
+@app.message("^parking info$")   
 def outputParkingInfo(say):
     text = "Number:         Status:\n"
-    for j in parkingInfoArray:
+    for j in parkingInfoArrayToday:
         if j.name == "empty":
           text += str(j.parkingNumber)+"                      available\n"
         else:
             if j.name =="":
-                text +=str(j.parkingNumber) + "                     reserved\n"
+                text +=str(j.parkingNumber) + "                     reserved, name not entered\n"
             else:
                 text +=str(j.parkingNumber) + "                     reserved by "+ j.name + "\n"
     say(text)
         
 # Start app
 if __name__ == "__main__":
+    #if the scheduled time is reached, switch today's info to tomorrow and restore tomorrow's info
+    if (datetime.datetime.now()==datetime.time(hour=23, minute=30)):
+       parkingInfoArrayToday = parkingInfoArrayTomorrow
+       parkingInfoArrayTomorrow = parkingInfoArrayEmpty
+    if (datetime.datetime.now()==datetime.time(hour=12, minute=00)):
+          result = client.chat_postMessage(
+          channel=channel1,
+          thread_ts=time.time(),
+          text="The parking reservation ended, enter 'parking info' to check your parking number!")
     SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
 
